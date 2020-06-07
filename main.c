20f6b4cb86e66983d89072413dfa1cb589dd0c41
David W. Arnold <d.w.arnold.gm@gmail.com>
2020-06-07 12:24:48 +0100

    Added flowchart2()

--- main.c
+++ main.c
@@ -28,15 +28,63 @@ int totalActivity(struct fitness *f);
 
 void printListItem(struct fitness *f);
 
+/**
+ * Exam 2018-19 1bi)
+ * The exam question asks to write the solution in main(), though it gets its own function for now.
+ */
+void flowchart2();
+
 int main() {
 
 //    flowchart();
 
-    struct fitness myFitnessLog[MAX_NUM];
-    int totalActivities = totalActivity(myFitnessLog);
-    printf("%d", totalActivities);
+//    struct fitness myFitnessLog[MAX_NUM];
+//    int totalActivities = totalActivity(myFitnessLog);
+//    printf("%d", totalActivities);
+
+    flowchart2();
 
     return 0;
+}
+
+/**
+ * Exam 2018-19 1bi)
+ * The exam question asks to write the solution in main(), though it gets its own function for now.
+ */
+void flowchart2() {
+    int n;
+    int i;
+    int j;
+    int k;
+    int flag = 0;
+    int isPrime1 = 1;
+    int isPrime2 = 1;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    for (i = 2; i <= n / 2; i++) {
+        isPrime1 = 1;
+        for (j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                isPrime1 = 0;
+            }
+        }
+        if (isPrime1 == 1) {
+            isPrime2 = 1;
+            for (k = 2; k <= (n - i) / 2; k++) {
+                if ((n - i) % k == 0) {
+                    isPrime2 = 0;
+                }
+            }
+            if (isPrime2 == 1) {
+                printf("%d = %d + %d\n", n, i, n - i);
+                flag = 1;
+            }
+        } //close if loop (isPrime1 == 1)
+    } //close for loop (i)
+    if (flag == 0) {
+        printf("%d cannot be expressed as the sum of two prime numbers.", n);
+    }
 }
 
 /**
@@ -52,7 +100,8 @@ int totalActivity(struct fitness *f) {
     char path[] = "/Users/David/Documents/CLion/CLion Projects/untitled/myActivity.txt";
     FILE *fin;
     if ((fin = fopen(path, "r")) != NULL) {
-        while (fscanf(fin, "%d %s %d %d", &f[i].dayNumber, f[i].activityName, &f[i].duration, &f[i].caloriesBurnt) == 4 && i < MAX_NUM) {
+        while (fscanf(fin, "%d %s %d %d", &f[i].dayNumber, f[i].activityName, &f[i].duration, &f[i].caloriesBurnt) ==
+               4 && i < MAX_NUM) {
             printf("%d %s %d %d\n", f[i].dayNumber, f[i].activityName, f[i].duration, f[i].caloriesBurnt);
             i++;
         }
